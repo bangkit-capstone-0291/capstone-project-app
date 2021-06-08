f0de0574577cc6ccb2fbaf0c09c2649cc25bc4af
1808561058_Ghani Rasyid Hika <64674997+Hikaaaaaa@users.noreply.github.com>
2021-06-09 03:19:45 +0800

    Login, Register, profile all about user

--- app/src/main/java/com/bangkit/electrateam/qualityumapp/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/bangkit/electrateam/qualityumapp/ui/SplashScreenActivity.kt
@@ -1,10 +1,12 @@
 package com.bangkit.electrateam.qualityumapp.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.bangkit.electrateam.qualityumapp.MainActivity
 import com.bangkit.electrateam.qualityumapp.R
+import com.bangkit.electrateam.qualityumapp.ui.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -16,6 +18,8 @@ class SplashScreenActivity : AppCompatActivity() {
         const val SPLASH_DELAY: Long = 2500
     }
 
+    lateinit var auth: FirebaseAuth
+
     private val scopeActivity = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,9 +29,16 @@ class SplashScreenActivity : AppCompatActivity() {
         scopeActivity.launch {
             delay(SPLASH_DELAY)
 
-            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+            auth = FirebaseAuth.getInstance()
+            val currentUsers = auth.currentUser
+            if (currentUsers != null) {
+                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+                finish()
+            } else {
+                val intent = Intent(this@SplashScreenActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
     }
 }
--- app/src/main/java/com/bangkit/electrateam/qualityumapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/bangkit/electrateam/qualityumapp/ui/profile/ProfileFragment.kt
@@ -1,21 +1,27 @@
 package com.bangkit.electrateam.qualityumapp.ui.profile
 
 import android.content.Intent
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.provider.Settings
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.bangkit.electrateam.qualityumapp.databinding.FragmentProfileBinding
+import com.bangkit.electrateam.qualityumapp.ui.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class ProfileFragment : Fragment() {
 
     private lateinit var viewModel: ProfileViewModel
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    lateinit var auth: FirebaseAuth
+    var databaseReference: DatabaseReference? = null
+    var database: FirebaseDatabase? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,19 +35,45 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database?.reference!!.child("profile")
+
+        loadProfile()
         binding.frameLayoutLanguage.setOnClickListener {
             val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
             startActivity(mIntent)
-        if (activity != null) {
+        }
 
-            binding.tvPersonalInformation.setOnClickListener {
-                val action =
-                    ProfileFragmentDirections.actionNavigationProfileToDetailProfileActivity()
-                action.let {
-                    findNavController().navigate(it)
-                }
+        binding.tvPersonalInformation.setOnClickListener {
+            val action =
+                ProfileFragmentDirections.actionNavigationProfileToDetailProfileActivity()
+            action.let {
+                findNavController().navigate(it)
             }
         }
+
+        binding.frameLayoutLogout.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+            activity?.finish()
+        }
+    }
+
+    private fun loadProfile() {
+        val user = auth.currentUser
+        val userReference = databaseReference?.child(user?.uid!!)
+
+        binding.tvProfileEmail.text = user?.email
+        userReference?.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.tvProfileName.text = snapshot.child("fullname").value.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 
     override fun onDestroyView() {
